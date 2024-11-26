796001e032cd20313f83d83331a80d06e27047be
robin <dpprobin@gmail.com>
2024-11-26 21:21:44 +0100

    scenes

--- script.js
+++ script.js
@@ -1,194 +1,193 @@
-const gridContainer = document.getElementById('grid-container');
-const player = document.getElementById('player')
+function menuFnc(){
 
-const screenW = screen.width
-const screenH = screen.height
-const rows = 11;
-const cols = 11;
-var inputX = "undefined";
-var inputY = "undefined";
-var posX = (screenW/2 - 70)
-var posY = (screenH/2 - 20)
-var terrainColors = []
-var terrainObject = []
-var lockedTerrainColors = []
+    document.querySelectorAll('.scene').forEach(scene => {
+        scene.classList.remove('active'); // Désactiver toutes les étapes
+    });
+    document.getElementById('menu').classList.add('active'); // Activer l'étape choisie
 
-
-function genTerrain(){
-    for (let i = 0; i < rows * cols; i++) {
-        randomColor = Math.floor(Math.random() * 4);
-        if (randomColor == 0){
-            randomColor = 'yellow'
-        }else{
-            randomColor = 'green'
+    document.getElementById('fullscreen-button').addEventListener('click', function() {
+        const element = document.documentElement; // L'élément racine <html>
+        if (element.requestFullscreen) {
+            element.requestFullscreen(); // Standard
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen(); // Firefox
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen(); // Chrome, Safari et Opera
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen(); // Internet Explorer/Edge
         }
-        // else if (randomColor == 2){
-        //     randomColor = 'blue'
-        // }else if (randomColor == 3){
-        //     randomColor = 'red'
-        // }else if (randomColor == 4){
-        //     randomColor = 'purple'
-        // }else if (randomColor == 5){
-        //     randomColor = 'violet'
-        // }else if (randomColor == 6){
-        //     randomColor = 'black'
-        // }
-        terrainColors.push(randomColor)
+
+        document.querySelectorAll('.scene').forEach(scene => {
+            scene.classList.remove('active'); // Désactiver toutes les étapes
+        });
+        document.getElementById('game').classList.add('active'); // Activer l'étape choisie
+
+        gameFnc()
+    });
+}
+
+
+function gameFnc(){
+    const gridContainer = document.getElementById('grid-container');
+    const player = document.getElementById('player')
+
+    const screenW = screen.width
+    const screenH = screen.height
+    const rows = 11;
+    const cols = 11;
+    var inputX = "undefined";
+    var inputY = "undefined";
+    var posX = (screenW/2 - 70)
+    var posY = (screenH/2 - 20)
+    var terrainColors = []
+    var terrainObject = []
+    var lockedTerrainColors = []
+
+
+    function genTerrain(){
+        for (let i = 0; i < rows * cols; i++) {
+            randomColor = Math.floor(Math.random() * 4);
+            if (randomColor == 0){
+                randomColor = 'yellow'
+            }else{
+                randomColor = 'green'
+            }
+            terrainColors.push(randomColor)
+
+
+            const gridItem = document.createElement('div');
+            gridItem.style.backgroundColor = terrainColors[i]
+            terrainObject.push(gridItem)
+            
+            gridContainer.appendChild(gridItem);
+        }  
+        movePlayer(posX, posY)
     }
-}
 
-genTerrain()
+    genTerrain()
 
-for (let i = 0; i < rows * cols; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.style.backgroundColor = terrainColors[i]
-    terrainObject.push(gridItem)
-    
-    // gridItem.addEventListener('click', () => {
+    document.addEventListener("mousedown", function(e){
+        startSwipe(e)
+    })
+    document.addEventListener("mouseup", function(e){
+        endSwipe(e)
+    })
+    document.addEventListener("touchstart", function(e){
+        startSwipe(e)
+    })
+    document.addEventListener("touchend", function(e){
+        endSwipe(e)
+    })
 
-    // });
-    
-    gridContainer.appendChild(gridItem);
-}
+    function startSwipe(input){
+        event.preventDefault();
+        inputX = input.clientX
+        inputY = input.clientY
+    }
 
-
-document.addEventListener("mousedown", function(e){
-    startSwipe(e)
-})
-document.addEventListener("mouseup", function(e){
-    endSwipe(e)
-})
-document.addEventListener("touchstart", function(e){
-    startSwipe(e)
-})
-document.addEventListener("touchend", function(e){
-    endSwipe(e)
-})
-
-function startSwipe(input){
-    event.preventDefault();
-    inputX = input.clientX
-    inputY = input.clientY
-}
-
-function endSwipe(input){
-    if (inputX != "undefined" && inputY != "undefined"){
-        if (Math.abs(input.clientX - inputX) > Math.abs(input.clientY - inputY)){
-            if (input.clientX - inputX < 0){
-                if (posX > screenW/2 - 70 - 150){
-                    posX -= 52
-                    movePlayer(posX, posY)
+    function endSwipe(input){
+        if (inputX != "undefined" && inputY != "undefined"){
+            if (Math.abs(input.clientX - inputX) > Math.abs(input.clientY - inputY)){
+                if (input.clientX - inputX < 0){
+                    if (posX > screenW/2 - 70 - 150){
+                        posX -= 52
+                        movePlayer(posX, posY)
+                    }   else{
+                        moveTerrain('right')
+                    }
                 }   else{
-                    moveTerrain('right')
+                    if (posX < screenW/2 - 70 + 150){
+                        posX += 52
+                        movePlayer(posX, posY)
+                    }   else{
+                        moveTerrain('left')
+                    }
                 }
             }   else{
-                if (posX < screenW/2 - 70 + 150){
-                    posX += 52
-                    movePlayer(posX, posY)
+                if (input.clientY - inputY < 0){
+                    if (posY > screenH/2 - 20 - 150){
+                        posY -= 52
+                        movePlayer(posX, posY)
+                    }   else{
+                        moveTerrain('up')
+                    }
                 }   else{
-                    moveTerrain('left')
+                    if (posY < screenH/2 - 20 + 150){
+                        posY += 52
+                        movePlayer(posX, posY)
+                    }   else{
+                        moveTerrain('down')
+                    }
                 }
             }
-        }   else{
-            if (input.clientY - inputY < 0){
-                if (posY > screenH/2 - 20 - 150){
-                    posY -= 52
-                    movePlayer(posX, posY)
-                }   else{
-                    moveTerrain('up')
+        }
+    }
+
+    function movePlayer(x, y){
+        player.style.left = x + 'px'
+        player.style.top = y + 'px'
+    }
+
+    function moveTerrain(dir){
+        lockedTerrainColors = []
+        for (let i = 0; i<terrainColors.length; i++){
+            lockedTerrainColors.push(terrainColors[i])
+        }    
+        
+        if (dir == "right"){
+            for (let i = 0; i < rows; i++){
+                for (let j = 0; j < cols; j++){
+                    if (j == 0){
+                        terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows + cols - 1]
+                        terrainColors[i * rows + j] = lockedTerrainColors[i * rows + cols - 1]
+                    }   else{
+                        terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows + j - 1]
+                        terrainColors[i * rows + j] = lockedTerrainColors[i * rows + j - 1]
+                    }
                 }
-            }   else{
-                if (posY < screenH/2 - 20 + 150){
-                    posY += 52
-                    movePlayer(posX, posY)
-                }   else{
-                    moveTerrain('down')
+            }
+        }
+        if (dir == "left"){
+            for (let i = 0; i < rows; i++){
+                for (let j = 0; j < cols; j++){
+                    if (j == cols-1){
+                        terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows]
+                        terrainColors[i * rows + j] = lockedTerrainColors[i * rows]
+                    }   else{
+                        terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows + j + 1]
+                        terrainColors[i * rows + j] = lockedTerrainColors[i * rows + j + 1]
+                    }
+                }
+            }
+        }
+        if (dir == "up"){
+            for (let i = 0; i < rows; i++){
+                for (let j = 0; j < cols; j++){
+                    if (i == 0){
+                        terrainObject[j].style.backgroundColor = lockedTerrainColors[rows * cols - cols + j]
+                        terrainColors[j] = lockedTerrainColors[rows * cols - cols + j]
+                    }   else{
+                        terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows + j - rows]
+                        terrainColors[i * rows + j] = lockedTerrainColors[i * rows + j - rows]
+                    }
+                }
+            }
+
+        }
+        if (dir == "down"){
+            for (let i = 0; i < rows; i++){
+                for (let j = 0; j < cols; j++){
+                    if (i == rows-1){
+                        terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[j]
+                        terrainColors[i * rows + j] = lockedTerrainColors[j]
+                    }   else{
+                        terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows + j + rows]
+                        terrainColors[i * rows + j] = lockedTerrainColors[i * rows + j + rows]
+                    }
                 }
             }
         }
     }
 }
 
-document.getElementById('fullscreen-button').addEventListener('click', activerPleinEcran);
-
-function activerPleinEcran() {
-    const element = document.documentElement; // L'élément racine <html>
-    if (element.requestFullscreen) {
-        element.requestFullscreen(); // Standard
-    } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen(); // Firefox
-    } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen(); // Chrome, Safari et Opera
-    } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen(); // Internet Explorer/Edge
-    }
-
-    movePlayer(posX, posY)
-}
-
-function movePlayer(x, y){
-    player.style.left = x + 'px'
-    player.style.top = y + 'px'
-}
-
-function moveTerrain(dir){
-    lockedTerrainColors = []
-    for (let i = 0; i<terrainColors.length; i++){
-        lockedTerrainColors.push(terrainColors[i])
-    }    
-    
-    if (dir == "right"){
-        for (let i = 0; i < rows; i++){
-            for (let j = 0; j < cols; j++){
-                if (j == 0){
-                    terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows + cols - 1]
-                    terrainColors[i * rows + j] = lockedTerrainColors[i * rows + cols - 1]
-                }   else{
-                    console.log(i * rows + j, i * rows + j - 1)
-                    terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows + j - 1]
-                    terrainColors[i * rows + j] = lockedTerrainColors[i * rows + j - 1]
-                }
-            }
-        }
-    }
-    if (dir == "left"){
-        for (let i = 0; i < rows; i++){
-            for (let j = 0; j < cols; j++){
-                if (j == cols-1){
-                    terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows]
-                    terrainColors[i * rows + j] = lockedTerrainColors[i * rows]
-                }   else{
-                    terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows + j + 1]
-                    terrainColors[i * rows + j] = lockedTerrainColors[i * rows + j + 1]
-                }
-            }
-        }
-    }
-    if (dir == "up"){
-        for (let i = 0; i < rows; i++){
-            for (let j = 0; j < cols; j++){
-                if (i == 0){
-                    terrainObject[j].style.backgroundColor = lockedTerrainColors[rows * cols - cols + j]
-                    terrainColors[j] = lockedTerrainColors[rows * cols - cols + j]
-                }   else{
-                    terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows + j - rows]
-                    terrainColors[i * rows + j] = lockedTerrainColors[i * rows + j - rows]
-                }
-            }
-        }
-
-    }
-    if (dir == "down"){
-        for (let i = 0; i < rows; i++){
-            for (let j = 0; j < cols; j++){
-                if (i == rows-1){
-                    terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[j]
-                    terrainColors[i * rows + j] = lockedTerrainColors[j]
-                }   else{
-                    terrainObject[i * rows + j].style.backgroundColor = lockedTerrainColors[i * rows + j + rows]
-                    terrainColors[i * rows + j] = lockedTerrainColors[i * rows + j + rows]
-                }
-            }
-        }
-    }
-}
+menuFnc()
